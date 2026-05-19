ace0d39b07f7d09500d25dbd3cd97c22cd2fe6e7
mohit6503 <mohitkumar42003@gmail.com>
2026-05-19 22:14:40 +0530

    static keyword

---
+++ StaticKeyword.java
@@ -0,0 +1,18 @@
+public class StaticKeyword {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// static keyword in java is used to share the same variable or method of a given class. 
+// It can be used with variables, methods, blocks and nested classes. The static keyword belongs to the 
+// class rather than any instance of the class. This means
+
+// non static members can be accessed only through objects, while static members can be accessed directly using the class name.
+
+// static member share the memory once 
+
+// static member can be access without creating object of the class. It can be accessed directly using class name and dot operator;
+
+
